7330ff19ae7844353b37a72e6cda900a8b3c34d8
Weilong <byegates@gmail.com>
2023-02-06 18:59:50 -0500

    Role.java: update equals to compare both id and name; RoleRepositoryTest.java: simplify autowire, Add DisplayNames, add beforeEach and set up a role in it. RoleServiceTest.java: Add DisplayNames, add update test case

--- src/test/java/com/wly/ecomm/service/RoleServiceTest.java
+++ src/test/java/com/wly/ecomm/service/RoleServiceTest.java
@@ -4,6 +4,7 @@ import com.wly.ecomm.exception.UserDefinedException;
 import com.wly.ecomm.model.Role;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -27,39 +28,44 @@ class RoleServiceTest {
         role = service.merge(new Role(roleName));
     }
 
-    @Test
+    @Test @DisplayName("Find all works fine")
     void findAll() {
         assertTrue(service.findAll().size() > 0);
     }
 
-    @Test
+    @Test @DisplayName("Find role by id")
     void findById() {
         var foundRole = service.findById(role.getId());
-        assertNotNull(foundRole);
-        assertEquals(role.getId(), foundRole.getId());
+        assertEquals(role, foundRole);
     }
 
-    @Test
+    @Test @DisplayName("Save Role successfully")
     void create_1NewRole() {
         assertNotNull(role);
     }
 
-    @Test
+    @Test @DisplayName("Role name is always saved in upper case")
     void create_ignore_case() {
         assertEquals("RoleServiceTest_findALL".substring(0, 20).toUpperCase(), role.getName());
     }
 
-    @Test
+    @Test @DisplayName("Duplicate role name will be ignored")
     void create_duplicateRole_Ignored() {
         var role2 = service.merge(new Role(roleName));
-        assertEquals(role.getId(), role2.getId());
-        assertEquals(role.getName(), role2.getName());
+        assertEquals(role, role2);
         assertEquals(roleName.toUpperCase(), role2.getName());
     }
 
-    @Test
+    @Test @DisplayName("delete a role by Id")
     void deleteById() {
         service.deleteById(role.getId());
         assertThrows(UserDefinedException.class, () -> service.findById(role.getId()));
+    }
+
+    @Test @DisplayName("Update name of saved Role")
+    void updateRoleName () {
+        role.setName("ROLEServiceTEST-U");
+        var updatedRole = service.merge(role);
+        assertEquals(role, updatedRole);
     }
 }
